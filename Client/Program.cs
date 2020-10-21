f68aba9afce468bf97bedb6f6e9e8a46098d16ce
dkStephanos <dkstephanos@gmail.com>
2020-10-21 18:29:47 -0400

    Lab completed through step 5

---
+++ Client/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketClient
+{
+
+    public static void StartClient(string name)
+    {
+        // Data buffer for incoming data.  
+        byte[] bytes = new byte[1024];
+
+        // Connect to a remote device.  
+        try
+        {
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try
+            {
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                // Encode the data string into a byte array.  
+                byte[] msg = Encoding.ASCII.GetBytes(name + "<EOF>");
+
+                // Send the data through the socket.  
+                int bytesSent = sender.Send(msg);
+
+                // Receive the response from the remote device.  
+                int bytesRec = sender.Receive(bytes);
+                string returnMsg = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                Console.WriteLine("Echoed test = {0}",
+                    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                if(returnMsg.Contains("server1")) {
+                    Console.WriteLine("Forwarding response to next server");
+                    Socket sender2 = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                    sender2.Connect(new IPEndPoint(ipAddress, Int32.Parse(returnMsg.Split(',')[1])));
+
+                    Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                    msg = Encoding.ASCII.GetBytes(returnMsg.Split(',')[0].Substring(returnMsg.Split(',')[0].Length - 8) + "<EOF>");
+
+                    sender2.Send(msg);
+                }
+
+
+
+                // Release the socket.  
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static int Main(String[] args)
+    {
+        Console.WriteLine("Enter name to send to server: ");
+        string name = Console.ReadLine();
+
+        StartClient(name);
+        return 0;
+    }
+}
---
+++ DNSLookup2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketListener
+{
+
+    // Incoming data from the client.  
+    public static string data = null;
+
+    public static void StartListening(string name, int port)
+    {
+        // Data buffer for incoming data.  
+        byte[] bytes = new Byte[1024];
+
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the
+        // host running the application.  
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+        // Create a TCP/IP socket.  
+        Socket listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            // Start listening for connections.  
+            while (true)
+            {
+                Console.WriteLine("Server " + name + " waiting for a connection on port " + port);
+                // Program is suspended while waiting for an incoming connection.  
+                Socket handler = listener.Accept();
+                data = null;
+
+                // An incoming connection needs to be processed.  
+                while (true)
+                {
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                string returnMessage = "";
+
+                // Show the data on the console.  
+                Console.WriteLine("Text received : {0}", data);
+
+                // If we are root, respond
+                if (name == "root")
+                {
+                    returnMessage = "server1:" + data.Substring(0, data.Length - 9);
+
+                    if (data.Contains(".com"))
+                    {
+                        returnMessage += ",5001";
+                    }
+                    else if (data.Contains(".edu"))
+                    {
+                        returnMessage += ",5002";
+                    }
+                }
+
+                // Echo the data back to the client.  
+                byte[] msg = Encoding.ASCII.GetBytes(returnMessage);
+
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static int Main(String[] args)
+    {
+        Console.WriteLine("Enter server name: ");
+        string serverName = Console.ReadLine();
+        int port = 11000;
+
+        switch (serverName)
+        {
+            case "root":
+                port = 5000;
+                break;
+            case "com":
+                port = 5001;
+                break;
+            case "edu":
+                port = 5002;
+                break;
+            case "fred":
+                port = 5003;
+                break;
+            case "wilma":
+                port = 5004;
+                break;
+            case "sensei":
+                port = 5005;
+                break;
+            case "senpai":
+                port = 5006;
+                break;
+        }
+
+        StartListening(serverName, port);
+        return 0;
+    }
+}
